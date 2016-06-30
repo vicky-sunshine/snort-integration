43d1c018d562c800285d182269a69fa03cbdd561
vicky <lch82327@gmail.com>
2016-06-30 15:24:31 +0800

    simple switch with traffic mirror

---
+++ mirror_switch.py
@@ -0,0 +1,114 @@
+from webob import Response
+from ryu.base import app_manager
+from ryu.app.wsgi import ControllerBase, WSGIApplication, route
+from ryu.controller import ofp_event
+from ryu.controller.handler import set_ev_cls
+from ryu.controller.handler import CONFIG_DISPATCHER, MAIN_DISPATCHER
+from ryu.lib.packet import packet
+from ryu.lib.packet import ethernet
+from ryu.lib.packet import ether_types
+
+from helper import ofp_helper
+from route import urls
+
+network_tap_instance_name = 'network_tap_api_app'
+
+# mirror_port, mirror all port traffic
+mirror_port = 48
+
+
+class NetworkTap(app_manager.RyuApp):
+    _CONTEXTS = {'wsgi': WSGIApplication}
+
+    def __init__(self, *args, **kwargs):
+        super(NetworkTap, self).__init__(*args, **kwargs)
+        self.switches = {}
+        wsgi = kwargs['wsgi']
+        wsgi.register(NetworkTapController,
+                      {network_tap_instance_name: self})
+        self.topology_api_app = self
+
+        @set_ev_cls(ofp_event.EventOFPSwitchFeatures, CONFIG_DISPATCHER)
+        def switch_features_handler(self, ev):
+            datapath = ev.msg.datapath
+            ofproto = datapath.ofproto
+            parser = datapath.ofproto_parser
+
+            # install table-miss flow entry
+            match = parser.OFPMatch()
+            actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                              ofproto.OFPCML_NO_BUFFER)]
+            ofp_helper.add_flow(datapath, 0, match, actions)
+
+        @set_ev_cls(ofp_event.EventOFPPacketIn, MAIN_DISPATCHER)
+        def _packet_in_handler(self, ev):
+            # If you hit this you might want to increase
+            # the "miss_send_length" of your switch
+            if ev.msg.msg_len < ev.msg.total_len:
+                self.logger.debug("packet truncated: only %s of %s bytes",
+                                  ev.msg.msg_len, ev.msg.total_len)
+            msg = ev.msg
+            datapath = msg.datapath
+            ofproto = datapath.ofproto
+            parser = datapath.ofproto_parser
+            in_port = msg.match['in_port']
+
+            pkt = packet.Packet(msg.data)
+            eth = pkt.get_protocols(ethernet.ethernet)[0]
+
+            if eth.ethertype == ether_types.ETH_TYPE_LLDP:
+                # ignore lldp packet
+                return
+            dst = eth.dst
+            src = eth.src
+
+            dpid = datapath.id
+            self.mac_to_port.setdefault(dpid, {})
+
+            self.logger.info("packet in %s %s %s %s", dpid, src, dst, in_port)
+
+            # learn a mac address to avoid FLOOD next time.
+            self.mac_to_port[dpid][src] = in_port
+
+            if dst in self.mac_to_port[dpid]:
+                out_port = self.mac_to_port[dpid][dst]
+            else:
+                out_port = ofproto.OFPP_FLOOD
+
+            if in_port == mirror_port:
+                # don't mirror snort traffic
+                actions = [parser.OFPActionOutput(out_port)]
+            else:
+                # add mirror port
+                actions = [parser.OFPActionOutput(out_port),
+                           parser.OFPActionOutput(mirror_port)]
+
+            # install a flow to avoid packet_in next time
+            if out_port != ofproto.OFPP_FLOOD:
+                match = parser.OFPMatch(in_port=in_port, eth_dst=dst)
+                # verify if we have a valid buffer_id, if yes avoid to send both
+                # flow_mod & packet_out
+                if msg.buffer_id != ofproto.OFP_NO_BUFFER:
+                    ofp_helper.add_flow(datapath, 1, match, actions, msg.buffer_id)
+                    return
+                else:
+                    ofp_helper.add_flow(datapath, 1, match, actions)
+
+            data = None
+            if msg.buffer_id == ofproto.OFP_NO_BUFFER:
+                data = msg.data
+
+            out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                      in_port=in_port, actions=actions, data=data)
+            datapath.send_msg(out)
+
+
+class NetworkTapController(ControllerBase):
+    def __init__(self, req, link, data, **config):
+        super(NetworkTapController, self).__init__(req, link, data, **config)
+        self.stat_monitor_spp = data[network_tap_instance_name]
+
+    @route('network_tap', urls.port_config, methods=['PUT'])
+    def hello(self, req, **kwargs):
+        # just a dump API
+        return Response(status=200)
---
+++ one_host_version/snort_firewall.py
@@ -0,0 +1,107 @@
+import json
+
+from webob import Response
+from ryu.base import app_manager
+from ryu.app.wsgi import ControllerBase, WSGIApplication, route
+from ryu.topology.api import get_switch
+from ryu.ofproto import ether
+from ryu.ofproto import inet
+from ryu.ofproto import ofproto_v1_3
+
+from route import urls
+from helper import ofp_helper
+
+snort_firewall_instance_name = 'snort_firewall_api_app'
+fw_priority = 32768
+
+
+class SnortFirewall(app_manager.RyuApp):
+    OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
+    _CONTEXTS = {'wsgi': WSGIApplication}
+
+    def __init__(self, *args, **kwargs):
+        super(SnortFirewall, self).__init__(*args, **kwargs)
+        self.switches = {}
+        wsgi = kwargs['wsgi']
+        wsgi.register(SnortFirewallController,
+                      {snort_firewall_instance_name: self})
+        self.topology_api_app = self
+
+    def add_block_rule(self, src_ip, dst_ip, protocol, dst_port, src_port):
+        switch_list = get_switch(self.topology_api_app, None)
+        for switch in switch_list:
+            datapath = switch.dp
+            parser = datapath.ofproto_parser
+
+            actions = []  # drop
+
+            # initial match field(IP layer)
+            match_dict = self.handle_match(src_ip, dst_ip, protocol,
+                                           dst_port, src_port)
+
+            match = parser.OFPMatch(**match_dict)
+            ofp_helper.add_flow(datapath, fw_priority, match, actions, 10)
+
+    def handle_match(self, src_ip, dst_ip, protocol, dst_port, src_port):
+        # initial match field(IP layer)
+        match_dict = {'eth_type': ether.ETH_TYPE_IP}
+
+        if src_ip:
+            match_dict.update({'ipv4_src': src_ip})
+
+        if dst_ip:
+            match_dict.update({'ipv4_dst': dst_ip})
+
+        if protocol:
+            match_dict.update({'ip_proto': protocol})
+
+        # fill into the layer3 and layer 4 protocol
+        if src_port:
+            if protocol == inet.IPPROTO_TCP:
+                match_dict.update({'tcp_src': src_port})
+            elif protocol == inet.IPPROTO_UDP:
+                match_dict.update({'udp_src': src_port})
+
+        if dst_port:
+            if protocol == inet.IPPROTO_TCP:
+                match_dict.update({'tcp_dst': dst_port})
+            elif protocol == inet.IPPROTO_UDP:
+                match_dict.update({'udp_dst': dst_port})
+
+        return match_dict
+
+
+class SnortFirewallController(ControllerBase):
+    def __init__(self, req, link, data, **config):
+        super(SnortFirewallController, self).__init__(req, link,
+                                                      data, **config)
+        self.snort_firewall_spp = data[snort_firewall_instance_name]
+
+    @route('firewall', urls.firewall_acl, methods=['PUT'])
+    def block_rule_customport(self, req, **kwargs):
+        try:
+            snort_firewall = self.snort_firewall_spp
+            request_tuple = json.loads(req.body)
+
+            src_ip = str(request_tuple.get('srcIP'))
+            dst_ip = str(request_tuple.get('dstIP'))
+            src_port = request_tuple.get('srcPort')
+            dst_port = request_tuple.get('dstPort')
+            protocol = str(request_tuple.get('protocol'))
+
+            if protocol == 'TCP':
+                protocol = inet.IPPROTO_TCP
+            elif protocol == 'UDP':
+                protocol = inet.IPPROTO_UDP
+            elif protocol == 'ICMP':
+                protocol = inet.IPPROTO_ICMP
+            else:
+                pass
+
+            snort_firewall.add_block_rule(src_ip, dst_ip, protocol,
+                                          dst_port, src_port)
+        except Exception as e:
+            print e
+            return Response(status=406)
+
+        return Response(status=202)
